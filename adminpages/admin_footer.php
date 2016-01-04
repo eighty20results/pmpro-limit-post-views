f963a84b5e1974e1d0c2afae6e11f61b448e85bc
Thomas Sjolshagen <thomas@eighty20results.com>
2016-01-04 05:26:18 -0500

    Enh: Styling for backend options page - Initial commit

---
+++ adminpages/admin_footer.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+ * Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+ */
+?>
+<div class="clear"></div>
+</div>
