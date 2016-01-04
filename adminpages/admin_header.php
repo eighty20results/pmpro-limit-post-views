967fb15ea4b94f0b34d0e10dbf17364123cdd120
Thomas Sjolshagen <thomas@eighty20results.com>
2016-01-04 05:25:57 -0500

    Enh: Styling for backend options page - Initial commit

---
+++ adminpages/admin_header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+* Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+*/
+
+$view = isset($_REQUEST['page'])? sanitize_text_field($_REQUEST['page']) : ""; ?>
+
+<div class="wrap e20rbpc-admin">
+	<div class="e20rbpc-banner">
+		<a class="e20r-logo" title="Eighty / 20 Results - Blur Protected Content Plugin for WordPress" target="_blank" href="https://eighty20results.com">
+            <img src="<?php echo E20R_BPC_PLUGIN_URL; ?>/images/Eighty20Results-Logo-small.png" height="75" border="0" alt="Eighty/20Results by Wicked Strong Chick, LLC (c) - All Rights Reserved" />
+        </a>
+        <div class="e20rbpc-meta">
+            <span class="e20rbpc-grey">v<?php echo "0.8.1"; ?></span>
+            <a target="_blank" class="e20rbpc-tag-green" href="https://eighty20results.zendesk.com"><?php _e('Plugin Support', 'e20rbpc');?></a>
+        </div>
+        <br style="clear:both;" />
+    </div>
+
+    <div id="e20rbpc-notifications">
+    </div>
+    <script>
+        jQuery(document).ready(function() {
+            jQuery.get('<?php echo get_admin_url(NULL, "/admin-ajax.php?action=e20rbpc_notifications"); ?>', function(data) {
+                if(data && data != 'NULL')
+                    jQuery('#e20rbpc_notifications').html(data);
+            });
+        });
+    </script>
+</div>
